09256c37f502cd25e8d18994cf0a07eebe88615d
[AI8/1반] 박현진 <dev.hyunjin.park@gmail.com>
2023-10-14 18:03:06 +0900

    마우스 인터랙션

--- 5-fireworks/src/main.js
+++ 5-fireworks/src/main.js
@@ -26,13 +26,24 @@ function init() {
 
   camera.position.z = 8000;
 
+  // 폭죽이 생성될 때마다 추가 -> 클릭시 업데이트 메서드 호출 용
+  const fireworks = [];
+  fireworks.update = function () {
+    for (let i = 0; i < this.length; i++) {
+      const firework = fireworks[i];
+
+      firework.update();
+    }
+  };
+
   const firework = new Firework({ x: 0, y: 0 });
   scene.add(firework.points);
+  fireworks.push(firework);
 
   render(); // 아래의 render 함수 호출
 
   function render() {
-    firework.update();
+    fireworks.update();
     renderer.render(scene, camera); // 새로 렌더한다
     requestAnimationFrame(render); // 재귀적으로 호출
   }
@@ -47,4 +58,16 @@ function init() {
   }
 
   window.addEventListener("resize", handleResize);
+
+  function handleMouseDown() {
+    const firework = new Firework({
+      x: THREE.MathUtils.randFloatSpread(8000), // -4000 ~ 4000 사이 랜덤
+      y: THREE.MathUtils.randFloatSpread(8000),
+    });
+
+    scene.add(firework.points);
+    fireworks.push(firework);
+  }
+
+  window.addEventListener("mousedown", handleMouseDown);
 }
