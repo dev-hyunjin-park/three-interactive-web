10c4bbecfe9e779aa6e75537e8d4bfb3d157076f
[AI8/1반] 박현진 <dev.hyunjin.park@gmail.com>
2023-10-14 16:51:50 +0900

    BufferGeometry로 파티클 효과 만들기

--- 5-fireworks/src/main.js
+++ 5-fireworks/src/main.js
@@ -28,7 +28,29 @@ function init() {
 
   new OrbitControls(camera, renderer.domElement);
 
-  const geometry = new THREE.SphereGeometry();
+  const geometry = new THREE.BufferGeometry();
+
+  const count = 10000; // 정점의 갯수
+
+  const positions = new Float32Array(count * 3); // 정점의 위치를 담을 배열
+  // 32비트 부동 소수점 숫자 배열. 각 정점의 x,y,z 좌표를 갖는 하나의 배열로 표현됨
+  // 배열의 크기는 count * 3
+
+  // 무작위로 정점의 위치를 설정
+  for (let i = 0; i < count; i++) {
+    // positions[i * 3] = Math.random() - 0.5; // -0.5~0.5 사이의 임의의 실수 값
+    // positions[i * 3 + 1]= Math.random() - 0.5;
+    // positions[i * 3 + 2]= Math.random() - 0.5;
+    positions[i * 3] = THREE.MathUtils.randFloatSpread(10); // 위와 같은 값을 Three.js 내장 함수를 사용해서 구할 수 있음
+    positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(10);
+    positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10);
+  }
+
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+  // 정점 위치 데이터를 BufferGeometry의 position 속성에 할당한다
+  // bufferAttribute: Three.js의 내장 클래스로, 정점 데이터를 버퍼에 저장하고 해당 버퍼를 geometry에 연결해준다
+  // position 속성은 정점의 위치 정보를 나타내며, 각 정점은 3개의 값(x,y,z)를 가지고 있음을 나타낸다
+
   const material = new THREE.PointsMaterial({
     color: 0xccaaff,
     size: 0.01,
