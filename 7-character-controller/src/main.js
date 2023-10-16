71a82dbda9259d50c139db0380e6c48726d70f40
박현진 <dev.hyunjin.park@gmail.com>
2023-10-16 12:06:12 +0900

    그림자 띄우기

--- 7-character-controller/src/main.js
+++ 7-character-controller/src/main.js
@@ -1,18 +1,21 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from "lil-gui";
 
 window.addEventListener("load", function () {
   init();
 });
 
 async function init() {
+  const gui = new GUI();
   const renderer = new THREE.WebGLRenderer({
     antialias: true, // 큐브에 계단현상 없애기(매끈하지 않은 표면)
   });
 
   // 캔버스 크기 조정
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
 
   document.body.appendChild(renderer.domElement); // 캔버스 요소(렌더러)를 domElem에 추가해준다
   // 3d 컨텐츠를 담을 씬
@@ -55,12 +58,75 @@ async function init() {
   const model = gltf.scene;
 
   model.scale.set(0.1, 0.1, 0.1);
+  model.traverse((object) => {
+    if (object.isMesh) {
+      object.castShadow = true;
+    }
+  });
   scene.add(model);
   camera.lookAt(model.position);
+
+  // 바닥 표현
+  const planeGeometry = new THREE.PlaneGeometry(10000, 10000, 10000);
+  const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0x000,
+  });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = -Math.PI / 2;
+  plane.position.y = -7.5;
+  plane.receiveShadow = true;
+  scene.add(plane);
 
   const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x333333);
   hemisphereLight.position.set(0, 20, 10);
   scene.add(hemisphereLight);
+
+  const spotlight = new THREE.SpotLight(
+    0xffffff,
+    28,
+    175,
+    Math.PI * 0.13,
+    0.5,
+    0.5
+  );
+
+  spotlight.position.set(0, 20, 0);
+
+  scene.add(spotlight);
+
+  spotlight.castShadow = true;
+  spotlight.shadow.mapSize.width = 1024;
+  spotlight.shadow.mapSize.height = 1024;
+  spotlight.shadow.radius = 8;
+
+  const spotlightFolder = gui.addFolder("Spotlight Settings");
+
+  // Spotlight 빛의 세기 설정
+  const intensityController = spotlightFolder
+    .add(spotlight, "intensity", 0, 200)
+    .name("Intensity");
+
+  // Spotlight 빛의 범위 설정
+  const distanceController = spotlightFolder
+    .add(spotlight, "distance", 0, 200)
+    .name("Distance");
+
+  // Spotlight 빛의 각도 설정
+  const angleController = spotlightFolder
+    .add(spotlight, "angle", 0, Math.PI)
+    .name("Angle");
+
+  intensityController.onChange(function (value) {
+    spotlight.intensity = value;
+  });
+
+  distanceController.onChange(function (value) {
+    spotlight.distance = value;
+  });
+
+  angleController.onChange(function (value) {
+    spotlight.angle = value;
+  });
 
   render(); // 아래의 render 함수 호출
 
